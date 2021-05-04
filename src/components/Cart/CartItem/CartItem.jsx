be7113f8d3e9e9ba119c5d9d5c327b554a0dd577
Haruna Oseni <harunaoseni23@gmail.com>
2021-05-04 16:45:08 -0500

    checkpoint

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -28,8 +28,8 @@ const styles = (theme) => ({
 
 class CartItem extends React.Component {
   render() {
-    const item = this.props;
-    const classes = this.props;
+    const { item } = this.props;
+    const { classes } = this.props;
     return (
       <Card className="cart-item">
         <CardMedia
@@ -43,6 +43,26 @@ class CartItem extends React.Component {
             {item.line_total.formatted_with_symbol}
           </Typography>
         </CardContent>
+        <CardActions className={classes.cardActions}>
+          <div className={classes.buttons}>
+            <Button type="button" size="small">
+              -
+            </Button>
+            <Typography>&nbsp;{item.quantity}&nbsp;</Typography>
+            <Button type="button" size="small">
+              +
+            </Button>
+          </div>
+          <div className={classes.buttons}>
+          <Button
+            variant="contained"
+            type="button"
+            color="secondary"
+          >
+            Remove
+          </Button>
+          </div>
+        </CardActions>
       </Card>
     );
   }
